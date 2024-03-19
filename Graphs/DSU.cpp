efeeb56048b69d1eddcb921fc8ce007cb8e09a1a
Tushar Parakh <21BCS4197@CUCHD.IN>
2024-03-19 15:16:14 +0530

    Added Union by Size

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 class disjointSet{
-    vector<int> parent, rank;
+    vector<int> parent, rank, size;
 
     public:
     //initializing the constructor
     disjointSet(int n){ 
       rank.resize(n+1, 0);
       parent.resize(n+1);
+      size.resize(n+1);
       for(int i=0; i<=n; i++){
         parent[i]=i;
+        size[i]=1;
       }
     }
     //The Find Ultimate Parent Function
@@ -35,6 +37,24 @@ class disjointSet{
         else{
             parent[ult_u]=ult_v;
             rank[ult_v]++;
+        }
+
+    }
+
+    // The Union by Size Function
+
+     void unionBySize(int u, int v){
+        int ult_u= findParent(u);
+        int ult_v= findParent(v);
+        if(ult_u==ult_v) return;
+        else if(size[ult_u]>size[ult_v]){
+          parent[ult_v]= parent[ult_u];
+          size[ult_u]+= size[ult_v];
+        }
+      
+        else{
+            parent[ult_u]=ult_v;
+            size[ult_v]+=size[ult_u];
         }
 
     }
